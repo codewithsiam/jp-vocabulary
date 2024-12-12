bf10d914d4f73d23ca4719a7d8cb391a57009549
Abu Sayed Al Siam <abusayedalsiam2023@gmail.com>
2024-12-12 23:19:31 +0600

    ui: admin and user layout

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,69 @@
+/* eslint-disable react/prop-types */
+import { createContext, useState, useContext, useEffect } from "react";
+import axiosPublic from "../lib/axiosPublic";
+import { toast } from "sonner";
+
+const AuthContext = createContext(undefined);
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkLoggedIn = async () => {
+      const token = localStorage.getItem("authToken");
+      if (token) {
+        try {
+          const res = await axiosPublic.get("/auth/me");
+          setUser(res.data.data.user);
+        } catch (error) {
+          console.error("Error checking authentication", error);
+          localStorage.removeItem("authToken");
+        }
+      }
+      setLoading(false);
+    };
+    checkLoggedIn();
+  }, []);
+
+  const login = async (email, password) => {
+    const res = await axiosPublic.post("/auth/login", { email, password });
+    console.log("resopnse from login api:",res);
+    if (res.data.success) {
+      localStorage.setItem("authToken", res.data.data.token);
+      toast.success(res.data.message);
+      setUser(res.data.data.user);
+      return res.data.data.user;
+    }
+  };
+
+  const logout = () => {
+    localStorage.removeItem("authToken");
+    setUser(null);
+  };
+
+  const register = async (name, email, password, photoUrl) => {
+    const formData = { name, email, password, photoUrl };
+
+    const res = await axiosPublic.post("/auth/register", formData);
+    console.log(res);
+    if (res.data.success) {
+      toast.success(res.data.message);
+      return true;
+    }
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, loading, login, logout, register }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
