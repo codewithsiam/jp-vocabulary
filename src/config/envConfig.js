d11f2435ddc9c90d9a767280554db73d82d0ed1e
Abu Sayed Al Siam <abusayedalsiam2023@gmail.com>
2024-12-13 00:49:16 +0600

    deploy

--- src/config/envConfig.js
+++ src/config/envConfig.js
@@ -1,6 +1,6 @@
 const envConfig = {
-  baseApi: "http://localhost:5000/api/v1",
-  // baseApi: "https://jp-vocabulary-server.vercel.app/api/v1",
+  // baseApi: "http://localhost:5000/api/v1",
+  baseApi: "https://jp-vocabulary-server.vercel.app/api/v1",
 };
 
 export default envConfig;
