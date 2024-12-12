dc9afc3701fd50c7577837021b6e8e40cefdd0b0
Abu Sayed Al Siam <abusayedalsiam2023@gmail.com>
2024-12-12 23:18:51 +0600

    feat: login and register

---
+++ src/components/form/UXForm.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable react/prop-types */
+
+import { FormProvider, useForm } from "react-hook-form";
+
+const UXForm = ({ children, onSubmit, defaultValues, resolver }) => {
+  console.log(defaultValues);
+  const formConfig = {};
+
+  if (defaultValues) {
+    formConfig.defaultValues = defaultValues;
+  }
+
+  if (resolver) {
+    formConfig.resolver = resolver;
+  }
+
+  const methods = useForm(formConfig);
+
+  const submitHandler = methods.handleSubmit;
+
+  return (
+    <FormProvider {...methods}>
+      <form className="px-6 py-8 space-y-6" onSubmit={submitHandler(onSubmit)}>
+        {children}
+      </form>
+    </FormProvider>
+  );
+};
+
+export default UXForm;
